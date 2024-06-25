a6c0fb31e34c8e9495fe8d2d41fd447f98d2c325
haldaniko <haldaniko@gmail.com>
2024-06-25 11:34:57 +0200

    delete 'Null=True' from class 'Task'

--- todo/models.py
+++ todo/models.py
@@ -13,7 +13,7 @@ class Task(models.Model):
     datetime = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField(null=True, blank=True)
     status = models.BooleanField(default=False)
-    tag = models.ManyToManyField(Tag, null=True, blank=True)
+    tag = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return self.content
